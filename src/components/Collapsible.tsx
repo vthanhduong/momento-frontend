95ec3b11ccb9db6d8c85139f4eca065f71662f8f
dungry853 <129316318+dungry853@users.noreply.github.com>
2025-03-04 12:03:46 +0700

    map folder

--- src/components/Collapsible.tsx
+++ src/components/Collapsible.tsx
@@ -1,11 +1,11 @@
 import { PropsWithChildren, useState } from "react";
 import { StyleSheet, TouchableOpacity } from "react-native";
 
-import { ThemedText } from "@/components/ThemedText";
-import { ThemedView } from "@/components/ThemedView";
-import { IconSymbol } from "@/components/ui/IconSymbol";
-import { Colors } from "@/src/constants/Colors";
-import { useColorScheme } from "@/hooks/useColorScheme";
+import { ThemedText } from "../components/ThemedText";
+import { ThemedView } from "../components/ThemedView";
+import { IconSymbol } from "../components/ui/IconSymbol";
+import { Colors } from "@constants/Colors";
+import { useColorScheme } from "@hooks/useColorScheme";
 
 export function Collapsible({
   children,
